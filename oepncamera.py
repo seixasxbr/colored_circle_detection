a4eb7777a6c50f20808a9a930299ec4fee700006
seixasxbr <mateus_seixas@hotmail.com.br>
2021-08-12 11:24:09 -0300

    add camera script

---
+++ oepncamera.py
@@ -0,0 +1,13 @@
+import cv2
+
+webcam = cv2.VideoCapture(0)
+
+if webcam.isOpened():
+    validacao, frame = webcam.read()
+    while validacao:
+        validacao, frame = webcam.read()
+        cv2.imshow("webcan", frame)
+        
+        key = cv2.waitKey(5)
+        if key == 27: #esc
+            break
